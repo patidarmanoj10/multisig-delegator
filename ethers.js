fb23486af7bb3f08710b719d281cddadeac5100e
Manoj <patidarmanoj@gmail.com>
2023-03-13 22:06:50 -0700

    Using mnemonic

--- ethers.js
+++ ethers.js
@@ -3,14 +3,12 @@ const ethers = require("ethers");
 const fetch = require("node-fetch");
 
 // Change below values.
-const TX_SERVICE_BASE_URL = "https://safe-transaction.goerli.gnosis.io/api/v1/delegates/";  // Goerli testnet
-let OWNER_PRIVATE_KEY = "...."
-const SAFE_ADDRESS = "0xB6F812c919c514f961118b968DcEEC6E2873Bf9d";
-const DELEGATE_ADDRESS = "0x76E111C8b6d2dCa50755B6e30cCDFE00b39d7a3B";
-
-let signer = new ethers.Wallet(OWNER_PRIVATE_KEY);
-
-// prepare message hash
+const TX_SERVICE_BASE_URL = "https://safe-transaction.mainnet.gnosis.io/api/v1/delegates/";  // Goerli testnet
+const SAFE_ADDRESS = "0xd1DE3F9CD4AE2F23DA941a67cA4C739f8dD9Af33";
+const DELEGATE_ADDRESS = "0x415344d56874eC6397Bf6Fe0075bb41E0086Aee5";
+const accountIndex = 5
+let signer = new ethers.Wallet.fromMnemonic(process.env.MNEMONIC, `m/44'/60'/0'/0/${accountIndex}`);
+signer.getAddress().then(console.log)
 const totp = (Date.now() / 1000 / 3600).toFixed(0);
 let msgHash = ethers.utils.solidityKeccak256([ "string" ], [ DELEGATE_ADDRESS + totp ]);
 let messageHashBinary = ethers.utils.arrayify(msgHash);
